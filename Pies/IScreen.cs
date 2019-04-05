5a8f152ef0fec5438f5b078911b5e8e5d66a90b4
Mateusz Tęgowski <m.tegowski@wp.pl>
2019-04-05 20:33:56 +0200

    Screen interface

---
+++ Pies/IScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pies
+{
+    interface IScreen
+    {
+        void Update(GameTime gameTime);
+        void Reset();
+        void Draw(SpriteBatch spriteBatch);
+        Rectangle rect();
+    }
+}
