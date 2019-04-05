65ece39d329bbab03041132bb86ed88a0aa37baf
Mateusz Tęgowski <m.tegowski@wp.pl>
2019-04-05 21:08:35 +0200

    Nie wiem co

---
+++ Pies/ScreenManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pies
+{
+    class ScreenManager
+    {
+        List<IScreen> screens;
+        int activeScreen;
+        int screenWidth, screenHeight;
+
+        public ScreenManager(int screenWidth, int screenHeight)
+        {
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+        }
+
+        public void LoadContent()
+        {
+            foreach(IScreen s in screens)
+            {
+                s.LoadContent();
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
