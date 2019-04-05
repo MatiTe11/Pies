71897177731ec0d4064da9d9b35e06d885f8c84e
mikron22 <mikron2222@gmail.com>
2019-04-05 21:33:52 +0200

    ,,

---
+++ Pies/TileDoors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Pies
+{
+    class TileDoors : Tile
+    {
+        public TileDoors(int X, int Y, int tilesize) : base(X, Y, tilesize) {}
+
+
+
+    }
+}
