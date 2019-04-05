f30ed5f927c948cdcbef2aca658cd34f89ca19e0
Deny <zmdddamian@gmail.com>
2019-04-05 22:06:44 +0200

    update

--- Pies/Player.cs
+++ Pies/Player.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 
 namespace Pies
@@ -14,23 +16,47 @@ namespace Pies
         protected int playerPositionX;
         protected int playerPositionY;
         public int playerSpeed;
+        public int sizeOfTile;
 
         protected int keyPressed;
 
         public Player() {}
 
-        public Player(int x, int y, int speed)
+        public Player(int x, int y, int speed, int sizeOfTile)
         {
             this.playerPositionX = x;     
             this.playerPositionY = y;
             this.playerSpeed = speed;
+            this.sizeOfTile = sizeOfTile;
         }
         public void Move(int x, int y)
+        {
+            this.playerPositionX = this.playerPositionX + x;
+            this.playerPositionY = this.playerPositionY + y;
+           
+        }
+        public void Update(GameTime gameTime)
         {
 
         }
 
+        public int PosX
+        {
+            get { return playerPositionX; }
+            set { playerPositionX = value; }
+        }
 
-        
+        public int PosY
+        {
+            get { return playerPositionY; }
+            set { playerPositionY = value; }
+        }
+
+        public int Speed
+        {
+            get { return playerSpeed; }
+            set { playerSpeed = value; }
+        }
+
     }
 }
