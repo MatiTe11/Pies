5bc9d0269dffdfb1ea4f21a16a08757cf32ebe00
Deny <zmdddamian@gmail.com>
2019-04-05 23:24:50 +0200

    Update move

--- Pies/Player.cs
+++ Pies/Player.cs
@@ -22,6 +22,11 @@ namespace Pies
         public int playerSpeed;
         public int sizeOfTile;
 
+        public bool downRight;
+        public bool upLeft;
+
+        public bool isMoving;
+
         public Player() {}
 
         public Player(int x, int y, int speed, int sizeOfTile)
@@ -32,36 +37,99 @@ namespace Pies
             this.sizeOfTile = sizeOfTile;
             this.changePositionX = 0;
             this.changePositionY = 0;
+            this.isMoving = false;
+            this.upLeft = false;
+            this.downRight = false;
         }
+
         public void Move(Direction direction)
         {
             if(direction == Direction.Up)
             {
-                this.changePositionY -= sizeOfTile;
+                this.changePositionY += sizeOfTile;
+                this.isMoving = true;
+                this.upLeft = true;
             }
             else if(direction == Direction.Down)
             {
                 this.changePositionY += sizeOfTile;
+                this.isMoving = true;
+                this.downRight = true;
             }
             else if (direction == Direction.Left)
             {
-                this.changePositionX -= sizeOfTile;
+                this.changePositionX += sizeOfTile;
+                this.isMoving = true;
+                this.upLeft = true;
             }
             else if (direction == Direction.Right)
             {
                 this.changePositionX += sizeOfTile;
+                this.downRight = true;
+                this.isMoving = true;
             }
 
         }
         public void Update(GameTime gameTime)
         {
-
+            if(this.IsMoving())
+            {
+                if(this.changePositionX > 0)
+                {
+                    if (this.downRight) //move right
+                    {
+                        this.playerPositionX += (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        this.changePositionX -= (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        if(changePositionX < 0)
+                        {
+                            this.playerPositionX -= this.changePositionX;
+                            this.changePositionX = 0;
+                        }
+                    }
+                    else //move left
+                    {
+                        this.playerPositionX -= (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        this.changePositionX -= (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        if (changePositionX < 0)
+                        {
+                            this.playerPositionX += this.changePositionX;
+                            this.changePositionX = 0;
+                        }
+                    }                          
+                }
+                else if(this.changePositionY > 0)
+                {
+                    if (this.downRight) //move down
+                    {
+                        this.playerPositionY += (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        this.changePositionY -= (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        if (changePositionY < 0)
+                        {
+                            this.playerPositionY -= this.changePositionY;
+                            this.changePositionY = 0;
+                        }
+                    }
+                    else //move up
+                    {
+                        this.playerPositionY -= (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        this.changePositionY -= (int)(playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                        if (changePositionY < 0)
+                        {
+                            this.playerPositionY += this.changePositionY;
+                            this.changePositionY = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    this.isMoving = false;
+                }
+            }
         }
 
-        public bool isMoving()
+        public bool IsMoving()
         {
-
-            return false;
+            return isMoving;
         }
 
         public int PosX
