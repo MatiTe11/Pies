c3638cecebca82522851d1ed9b5ac6a114dc33f4
Mateusz Tęgowski <m.tegowski@wp.pl>
2019-04-06 09:13:03 +0200

    Tile is enum now

--- Pies/GameScreen.cs
+++ Pies/GameScreen.cs
@@ -12,7 +12,7 @@ namespace Pies
     class GameScreen : Screen
     {
         private Player player;
-        private Dog dog;
+        //private Dog dog;
         private const int sizeOfBoardX = 3;
         private const int sizeOfBoardY = 3;
 
@@ -28,7 +28,6 @@ namespace Pies
 
         public GameScreen(int screenWidth, int screenHeight) : base(screenWidth,screenHeight)
         {
-            
             inputManager = new InputManager();
             tiles = new List<List<Tile>>();
 
@@ -58,7 +57,7 @@ namespace Pies
             emptyTex = Content.Load<Texture2D>("empty");
             this.textureScale =  (float)this.sizeOfTile / (float)emptyTex.Width;
             player = new Player(10, 10, 0.5f, sizeOfTile);
-            dog = new Dog(10, 10, 0.5f, 0, sizeOfTile, tiles, new List<Shit>());
+            //dog = new Dog(10, 10, 0.5f, 0, sizeOfTile, tiles, new List<Shit>());
         }
 
         override public void Reset()
@@ -70,7 +69,8 @@ namespace Pies
         {
             inputManager.Update();
             player.Update(gameTime);
-            dog.Update(gameTime, new List<Shit>());
+            //dog.Update(gameTime, new List<Shit>());
+            Tile currentTile = tiles[GetTileNumber(player.PosX)][GetTileNumber(player.PosY)];
 
             if (inputManager.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Left))
             {
@@ -88,14 +88,14 @@ namespace Pies
             }
             else if (inputManager.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Up))
             {
-                if (!player.isMoving && tiles[GetTileNumber(player.PosX)][GetTileNumber(player.PosY)] is TileStairs && GetTileNumber(player.PosY) > 0)
+                if (!player.isMoving && (currentTile == Tile.StairsWall || currentTile == Tile.StairsNoWall) && GetTileNumber(player.PosY) > 0)
                 {
                     player.Move(Direction.Up);
                 }
             }
             else if (inputManager.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Down))
             {
-                if (!player.isMoving && tiles[GetTileNumber(player.PosX)][ GetTileNumber(player.PosY)] is TileStairs && GetTileNumber(player.PosY) < (sizeOfBoardY-1))
+                if (!player.isMoving && ((currentTile == Tile.StairsWall || currentTile == Tile.StairsNoWall)) && GetTileNumber(player.PosY) < (sizeOfBoardY-1))
                 {
                     player.Move(Direction.Down);
                 }
@@ -126,11 +126,11 @@ namespace Pies
             {
                 for (int j = 0; j < sizeOfBoardY; j++)
                 {
-                    if (tiles[i][j] is TileDoors)
+                    if (tiles[i][j] == Tile.Door1 || tiles[i][j] == Tile.Door2 || tiles[i][j] == Tile.Door2)
                         spriteBatch.Draw(doorTex, new Vector2(firstTailPositionX + i * sizeOfTile, firstTailPositionY + j * sizeOfTile), null, Color.White, 0f, new Vector2(0,0), new Vector2(textureScale), SpriteEffects.None, 0f);
-                    else if (tiles[i][j] is TileStairs)
+                    else if (tiles[i][j] == Tile.StairsNoWall || tiles[i][j] == Tile.StairsWall)
                         spriteBatch.Draw(elevatorTex, new Vector2(firstTailPositionX + i * sizeOfTile, firstTailPositionY + j * sizeOfTile), null, Color.White, 0f, new Vector2(0, 0), new Vector2(textureScale), SpriteEffects.None, 0f);
-                    else if (tiles[i][j] is TileEmpty)
+                    else if (tiles[i][j] ==Tile.Empty)
                         spriteBatch.Draw(emptyTex, new Vector2(firstTailPositionX + i * sizeOfTile, firstTailPositionY + j * sizeOfTile), null, Color.White, 0f, new Vector2(0, 0), new Vector2(textureScale), SpriteEffects.None, 0f);
                 }
             }
@@ -148,11 +148,13 @@ namespace Pies
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    elem.Add(new TileStairs(0, 0, 0));
+                    elem.Add(Tile.Door1);
                 }
                 
             }
-            tiles[1][1] = new TileDoors(0, 0, 0);
+            tiles[1][0] = Tile.StairsWall;
+            tiles[1][2] = Tile.StairsWall;
+            tiles[1][2] = Tile.StairsWall;
 
         }
     }
--- Pies/Tile.cs
+++ Pies/Tile.cs
@@ -10,50 +10,8 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Pies
 {
-    abstract class Tile
+    enum Tile
     {
-        private Rectangle rec = new Rectangle();
-        private Point pos = new Point();
-        public Tile(int X, int Y, int tilesize)
-        {
-            pos.X = X;
-            pos.Y = Y;
-            rec.X = X * tilesize;
-            rec.Y = Y * tilesize;
-            rec.Height = tilesize;
-            rec.Width = tilesize;
-        }
-
-
-        public int PosX
-        {
-            get { return pos.X; }
-            set { pos.X = value; }
-        }
-        public int PosY
-        {
-            get { return pos.Y; }
-            set { pos.Y = value; }
-        }
-        public int RecX
-        {
-            get { return rec.X; }
-            set { rec.X = value; }
-        }
-        public int RecY
-        {
-            get { return rec.Y; }
-            set { rec.Y = value; }
-        }
-        public int RecSize
-        {
-            get { return rec.Height; }
-            set
-            {
-                rec.Height = value;
-                rec.Width = value;
-            }
-        }
-
-    }
+        Empty, StairsWall,StairsNoWall, Door1, Door2, Door3 
+    };
 }
